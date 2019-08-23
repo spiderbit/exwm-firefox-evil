7c2152ea9156a5fc14b3fb4bb700d691ed184e90
Stefan Huchler <stefan.huchler@mail.de>
2019-08-24 00:30:21 +0200

    Cleans up more code less DRY / commented out code

--- exwm-firefox-fly-keys.el
+++ exwm-firefox-fly-keys.el
@@ -31,15 +31,76 @@
 
 (require 'exwm)
 (require 'exwm-firefox-core)
+(require 'xah-fly-keys)
 
 (defvar exwm-firefox-class-name '("Firefox" "Nightly" "Iceweasel" "Icecat")
   "The class name used for detecting if a firefox buffer is selected.")
 
-(defvar exwm-firefox-fly-keys-mode-map (make-sparse-keymap))
-(defvar exwm-firefox-fly-e-keymap (make-sparse-keymap))
+(defvar exwm-firefox-fly-keys-mode-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map xah-fly-key-map)
+    (define-key map (kbd "m") 'kodi-remote-music)
+    (define-key map (kbd "<menu> e") exwm-firefox-fly-e-keymap)
+    (define-key map (kbd "e") 'exwm-edit--compose)
+    (define-key map (kbd "<home>") 'exwm-firefox-fly-keys-normal)
+    (define-key map (kbd "u") 'exwm-firefox-fly-keys-insert)
+    ;; Basic movements
+    (define-key map (kbd "t") 'exwm-firefox-core-down)
+    (define-key map (kbd "c") 'exwm-firefox-core-up)
+    ;; Search
+    (define-key map (kbd "<tab>") 'exwm-firefox-core-focus-search-bar)
+    ;; Send enter to firefox
+    (define-key map (kbd "<return>")
+      '(lambda () (interactive) (exwm-input--fake-key 'return)))
+    (define-key map (kbd "RET")
+      '(lambda () (interactive) (exwm-input--fake-key 'return)))
+    ;; Move by half page
+    (define-key map (kbd "d") 'exwm-firefox-core-half-page-up)
+    (define-key map (kbd "s") 'exwm-firefox-core-half-page-down)
+    ;; Reload page
+    (define-key map (kbd "<C-r>") 'exwm-firefox-core-reload)
+    (define-key map (kbd "<C-R>") 'exwm-firefox-core-reload-override-cache)
+    ;; History
+    (define-key map (kbd "n") 'exwm-firefox-core-history-forward)
+    (define-key map (kbd "h") 'exwm-firefox-core-history-back)
+    ;; Find
+    (define-key map (kbd "<menu> g") 'exwm-firefox-core-quick-find)
+    (define-key map (kbd "C-s") 'exwm-firefox-core-find-next)
+    (define-key map (kbd "C-r") 'exwm-firefox-core-find-previous)
+    ;; Editing
+    (define-key map (kbd "r") 'exwm-firefox-core-forward-word)
+    (define-key map (kbd "g") 'exwm-firefox-core-back-word)
+    (define-key map (kbd "k") 'exwm-firefox-core-paste)
+    (define-key map (kbd "j") 'exwm-firefox-core-copy)
+    (define-key map (kbd "q") 'exwm-firefox-core-cut)
+    (define-key map (kbd "p")
+      (lambda () (interactive)
+	(exwm-firefox-core-forward-word-select)
+	(exwm-firefox-core-delete)))
+    (define-key map (kbd ".")
+      (lambda () (interactive)
+	(exwm-firefox-core-back-word-select)
+	(exwm-firefox-core-delete)))
+    (define-key map (kbd "8")
+      (lambda () (interactive) (exwm-firefox-core-back-word)
+	(exwm-firefox-core-forward-word-select)))
+    (define-key map (kbd "f") 'exwm-firefox-core-undo)
+    ;; Select all and stop user from entering visual and insert state
+    (define-key map (kbd "<menu> a") 'exwm-firefox-core-select-all)
+    (define-key map (kbd "C-a") 'exwm-firefox-core-select-all)
+    ;; Pass through esc when in normal mode
+    (define-key map (kbd "<escape>") 'exwm-firefox-core-cancel)
+    ;; hint mode / copy url (needs firefox addon)
+    (define-key map (kbd "<menu> j")
+      '(lambda () (interactive) (dotimes (i 2) (exwm-input--fake-key 'y))))
+    map)
+  "Keymap for `exwm-firefox-fly-keys-mode'.")
 
-;; (defvar exwm-firefox-evil-insert-on-new-tab t
-;;   "If non-nil, auto enter insert mode after opening new tab.")
+(defvar exwm-firefox-fly-e-keymap
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map xah-fly-e-keymap)
+    (define-key map (kbd "e") 'exwm-edit--compose)
+    map))
 
 ;;; State transitions
 (defun exwm-firefox-fly-keys-normal ()
@@ -53,101 +114,6 @@
   (interactive)
   (setq-local exwm-input-line-mode-passthrough nil)
   (xah-fly-insert-mode-activate))
-
-;;; Keys
-(set-keymap-parent exwm-firefox-fly-keys-mode-map xah-fly-key-map)
-(set-keymap-parent exwm-firefox-fly-e-keymap xah-fly-e-keymap)
-
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<menu> e") exwm-firefox-fly-e-keymap)
-(define-key exwm-firefox-fly-e-keymap (kbd "e") 'exwm-edit--compose)
-;; (define-key xah-fly-key-map [remap previous-line] 'exwm-firefox-core-up)
-;; (define-key exwm-firefox-fly-keys-mode-map [remap next-line] 'exwm-firefox-core-down)
-;; (define-key xah-fly-key-map [remap backward-char] 'exwm-firefox-core-left)
-;; (define-key xah-fly-key-map [remap forward-char] 'exwm-firefox-core-right)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<home>") 'exwm-firefox-fly-keys-normal)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "u") 'exwm-firefox-fly-keys-insert)
-;; (define-key exwm-firefox-fly-keys-mode-map (kbd "<SPC>") 'exwm-firefox-fly-keys-insert)
-;; (define-key exwm-firefox-fly-keys-mode-map (kbd "<SPC>") nil)
-
-    ;;;; Normal
-;; Basic movements
-(define-key exwm-firefox-fly-keys-mode-map (kbd "t") 'exwm-firefox-core-down)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "c") 'exwm-firefox-core-up)
-;; (define-key exwm-firefox-fly-keys-mode-map (kbd "h") 'exwm-firefox-core-left)
-;; (define-key exwm-firefox-fly-keys-mode-map (kbd "n") 'exwm-firefox-core-right)
-
-;; Search
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<tab>") 'exwm-firefox-core-focus-search-bar)
-
-    ;;;; Transitions
-;; Bind normal
-;; (define-key xah-fly-key-map [remap xah-fly-command-mode-activate] 'xah-fly-command-mode-activate)
-;; (define-key exwm-firefox-evil-mode-map [remap evil-normal-state] 'exwm-firefox-evil-normal)
-;; (define-key exwm-firefox-evil-mode-map [remap evil-force-normal-state] 'exwm-firefox-evil-normal)
-;; Bind insert
-;; (define-key xah-fly-key-map [remap xah-fly-insert-mode-activate] 'exwm-firefox-fly-keys-insert)
-;; (define-key exwm-firefox-evil-mode-map [remap evil-insert] 'exwm-firefox-evil-insert)
-;; (define-key exwm-firefox-evil-mode-map [remap evil-substitute] 'exwm-firefox-evil-insert)
-;; (define-key exwm-firefox-evil-mode-map [remap evil-append] 'exwm-firefox-evil-insert)
-
-;; Send enter to firefox
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<return>")
-  '(lambda () (interactive) (exwm-input--fake-key 'return)))
-(define-key exwm-firefox-fly-keys-mode-map (kbd "RET")
-  '(lambda () (interactive) (exwm-input--fake-key 'return)))
-
-;; Move by half page
-(define-key exwm-firefox-fly-keys-mode-map (kbd "d") 'exwm-firefox-core-half-page-up)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "s") 'exwm-firefox-core-half-page-down)
-
-;; ;; Move to top/bot
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "g g") 'exwm-firefox-core-top)
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "G") 'exwm-firefox-core-bottom)
-
-;; Reload page
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<C-r>") 'exwm-firefox-core-reload)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<C-R>") 'exwm-firefox-core-reload-override-cache)
-;; History
-(define-key exwm-firefox-fly-keys-mode-map (kbd "n") 'exwm-firefox-core-history-forward)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "h") 'exwm-firefox-core-history-back)
-
-;; Find
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<menu> g") 'exwm-firefox-core-quick-find)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "C-s") 'exwm-firefox-core-find-next)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "C-r") 'exwm-firefox-core-find-previous)
-
-;; Editing
-(define-key exwm-firefox-fly-keys-mode-map (kbd "r") 'exwm-firefox-core-forward-word)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "g") 'exwm-firefox-core-back-word)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "k") 'exwm-firefox-core-paste)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "j") 'exwm-firefox-core-copy)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "q") 'exwm-firefox-core-cut)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "p") (lambda () (interactive) (exwm-firefox-core-forward-word-select)(exwm-firefox-core-delete)))
-(define-key exwm-firefox-fly-keys-mode-map (kbd ".") (lambda () (interactive) (exwm-firefox-core-back-word-select)(exwm-firefox-core-delete)))
-(define-key exwm-firefox-fly-keys-mode-map (kbd "8") (lambda () (interactive) (exwm-firefox-core-back-word)(exwm-firefox-core-forward-word-select)))
-
-(define-key exwm-firefox-fly-keys-mode-map (kbd "f") 'exwm-firefox-core-undo)
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "C-r") 'exwm-firefox-core-redo)
-;; (define-key exwm-firefox-fly-keys-mode-map (kbd "e") 'exwm-firefox-core-delete)
-
-;; Select all and stop user from entering visual and insert state
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<menu> a") 'exwm-firefox-core-select-all)
-(define-key exwm-firefox-fly-keys-mode-map (kbd "C-a") 'exwm-firefox-core-select-all)
-
-;; Pass through esc when in normal mode
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<escape>") 'exwm-firefox-core-cancel)
-
-;; hint mode / copy url (needs firefox addon)
-;; (define-key exwm-firefox-fly-keys-mode-map (kbd "y") '(lambda () (interactive) (exwm-input--fake-key 'u)(exwm-firefox-fly-keys-insert)))
-(define-key exwm-firefox-fly-keys-mode-map (kbd "<menu> j") '(lambda () (interactive) (dotimes (i 2) (exwm-input--fake-key 'y))))
-
-;; ;; Tab movement
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "J") 'exwm-firefox-core-tab-next)
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "K") 'exwm-firefox-core-tab-previous)
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "x") 'exwm-firefox-core-tab-close)
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "t") 'exwm-firefox-core-tab-new)
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "0") 'exwm-firefox-core-tab-first)
-;; (evil-define-key 'normal exwm-firefox-evil-mode-map (kbd "$") 'exwm-firefox-core-tab-last)
 
 ;;; Mode
 ;;;###autoload
